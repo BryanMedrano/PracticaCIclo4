43c5d849b7a61d1d4ca738b5e52472ebb8ef6f5c
Bryan Medrano <medranoortegabryan@gmail.com>
2021-11-08 11:58:40 -0500

    nuevos cambios

--- practica-c4/src/App.jsx
+++ practica-c4/src/App.jsx
@@ -1,15 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import 'styles/estilos.css'
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route to='login'>
-      
-        </Route>
-      </Switch>
-    </Router>
+  <div></div>
   );
 }
 

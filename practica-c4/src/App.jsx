0dddb2db4fcd8eb935c0d3d14ea8b3ef4b114f7b
Bryan Medrano <medranoortegabryan@gmail.com>
2021-11-08 16:32:12 -0500

    Corregir cambios en el router dom

--- practica-c4/src/App.jsx
+++ practica-c4/src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Login from './pages/Login';
+import Registro from "./pages/Registro";
+import Admin from './pages/Admin';
+import Index from './pages/Index';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
-  <div></div>
+    <Router>
+      <Router>
+        <Route path= '/login'>
+          <Login/>
+        </Route>
+        <Route path='/registro'>
+          <Registro/>
+        </Route>
+        <Route path='/admin'>
+          <Admin/>
+        </Route>
+        <Route path='/'>
+          <Index/>
+        </Route>
+      </Router>
+ </Router>
   );
 }
 
